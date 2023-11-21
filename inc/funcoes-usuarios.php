0083b3909620dd9a412ab8d12d4862d422db706d
nagarcia94 <000nagarcia@gmail.com>
2023-11-21 14:44:27 -0300

    leitura dos dados

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -11,6 +11,19 @@ function inserirUsuario($conexao, $nome, $email, $senha, $tipo){
    /* Executando o comando SQL  */
    mysqli_query($conexao, $sql) or die (mysqli_errno($conexao));
 
+
+
 }
 
+function lerUsuarios($conexao){
+    /* Comando SQL para fazer a leitura de dados (select) */
+    $sql = "SELECT id, nome, email, tipo FROM usuarios ORDER BY nome";
+
+    // Execução do comando e aramzenamento do resultado da consulta/query
+    $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+    // Retornamos o resultado da query transformando em array associativo
+    return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+
+}
 
