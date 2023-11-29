ffb04494af0965dd3a127b866846b9821212584a
nagarcia94 <000nagarcia@gmail.com>
2023-11-29 15:30:47 -0300

    usuario exclui

--- admin/noticia-exclui.php
+++ admin/noticia-exclui.php
@@ -1,3 +1,21 @@
 <?php 
-require "funcoes-sessao.php";
+require "../inc/funcoes-noticias.php";
+require "../inc/funcoes-sessao.php";
+
 verificaAcesso();
+
+$idNoticia = $_GET['id'];
+
+$idUsuario = $_SESSION['id'];
+$tipoUsuario = $_SESSION['tipo'];
+
+$noticia = excluirNoticia ($conexao, $idNoticia, $idUsuario, $tipoUsuario);
+
+    
+    header ("location:noticias.php");
+
+
+
+    
+
+
